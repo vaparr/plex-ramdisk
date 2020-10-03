6916c1c6e4915a455b504886753d3c47ddd78c7d
vaparr <vaparr>
2020-10-03 01:28:21 -0400

    Added Stop function

--- plex-ramdisk.sh
+++ plex-ramdisk.sh
@@ -1,6 +1,15 @@
 RAMDISKDIR=/dev/shm/plex_db_ramdisk
 PLEXDBLOC="/mnt/user/appdata/plex/Library/Application Support/Plex Media Server/Plug-in Support/Databases/"
 
+
+function Help(){
+
+    echo "-h,--help:  Displays this"
+    echo "--start  :  Starts the ramdisk"
+    echo "--stop   :  Stops the ramdisk"
+    exit 1
+}
+
 function FailExit(){
 
 umount "$PLEXDBLOC"
@@ -8,6 +17,9 @@ docker start plex
 
 exit 1
 }
+
+
+function Start(){
 
 
 if [ ! -d "$RAMDISKDIR" ]; then
@@ -22,7 +34,7 @@ if [ -f "$PLEXDBLOC/THIS_IS_A_RAMDISK" ]; then
 fi
 
 docker stop plex
-rsync -a --progress -h "$PLEXDBLOC" "$RAMDISKDIR"
+rsync -a --progress -h "$PLEXDBLOC/" "$RAMDISKDIR/"
 if [ "$?" != "0" ]; then
    echo rsync failed. Exiting
    FailExit
@@ -49,5 +61,63 @@ else
    FailExit
 fi
 
+}
 
+function Stop() {
+
+mountpoint "$PLEXDBLOC"
+
+if [ "$?" != "0" ]; then
+   echo $PLEXDBLOC is not a mountpoint.  Nothing to stop.
+   FailExit
+fi
+
+docker stop plex
+
+umount "$PLEXDBLOC"
+
+mountpoint "$PLEXDBLOC"
+
+
+if [ "$?" == "0" ]; then
+   echo $PLEXDBLOC is still a  mountpoint after unmounting. Something went wrong.
+   FailExit
+fi
+
+rsync -a --progress -h --exclude THIS_IS_A_RAMDISK "$RAMDISKDIR/" "$PLEXDBLOC/"
+
+docker start plex
+
+
+}
+
+
+
+TEMP=`getopt -o ?h --long start,stop,help  -n 'plex-ramdisk' -- "$@"`
+
+if [ $? != 0 ] ; then Help ; fi
+
+eval set -- "$TEMP"
+
+while true ; do
+    case "$1" in
+        "--start") echo "Starting the Ramdisk"
+            Start
+            break
+            ;;
+        "--stop") echo "Stop" 
+            Stop
+            break
+            ;;
+        -?|-h|--help) 
+            Help 
+            ;;
+        --) shift ; break ;;
+        *) Help
+            ;;
+    esac
+done
+#echo "Remaining arguments:"
+#for arg do echo '--> '"\`$arg'" ; done
+exit 2
 
